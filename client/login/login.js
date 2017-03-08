322219f9312464d97253b069db6bed863a299484
sigvartmh <sigvart.m@gmail.com>
2017-03-08 08:20:11 +0100

    Add login template Add SMS send form Add password check function on server

--- client/login/login.js
+++ client/login/login.js
@@ -1,6 +1,20 @@
-Template.users.helpers({
-  login() {
-    Users.find({username: phone_number})
-    return ;
+Template.secure_login.helpers({
+  login(phone_number) {
+    user = Users.find({username: phone_number})
+    console.log(user);
+    return user;
   },
 });
+
+Template.secure_login.events({
+  'submit .login'(event){
+    event.preventDefault();
+    const target = event.target;
+    const password = target.password.value;
+    const phone_number = target.phone_number.value;
+    Meteor.call('user.login',phone_number,password);
+    target.password.value = '';
+    target.phone_number.value = '';
+  //Meteor.call('sms.send',phone_number,text);
+}
+})
